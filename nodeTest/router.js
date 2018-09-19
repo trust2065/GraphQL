bd0fd1970d475b0fab0f2f31fdcd0633439da64f
Choco Li <cli@modernstar.com>
2018-09-19 17:12:17 +1000

    able to handle POST

--- nodeTest/router.js
+++ nodeTest/router.js
@@ -1,7 +1,7 @@
-function route(pathName, handle, response) {
+function route(pathName, handle, response, postData) {
     console.log("About to route a request for " + pathName);
     if (typeof handle[pathName] === 'function') {
-        handle[pathName](response);
+        handle[pathName](response, postData);
     } else {
         console.log("No request handler found for " + pathName);
         response.writeHead(404, { "Content-Type": "text/plain" });
--- nodeTest/server.js
+++ nodeTest/server.js
@@ -1,19 +1,30 @@
 var http = require("http");
 var url = require("url");
 
-
 function start(route, handle) {
     function onRequest(request, response) {
-        var pathName = url.parse(request.url).pathname;
-        if (pathName !== '/favicon.ico') {
-            console.log(`Request for ${pathName} received`);
+        var postData = "";
+        var pathname = url.parse(request.url).pathname;
+        if (pathname !== '/favicon.ico') {
+            console.log(`Request for ${pathname} received`);
 
-            route(pathName, handle, response);
+            request.setEncoding("utf8");
+
+            request.addListener("data", function (postDataChunk) {
+                postData += postDataChunk;
+                console.log("Received POST data chunk '" +
+                    postDataChunk + "'.");
+            });
+
+            request.addListener("end", function () {
+                console.log(`end`);
+                route(pathname, handle, response, postData);
+            });
+
         }
     }
 
     http.createServer(onRequest).listen(8888);
-
     console.log("Server has started.");
 }
 
