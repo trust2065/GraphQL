9386ff8f37ce4246007c09b659e775ecee61475b
Choco Li <cli@modernstar.com>
2018-09-19 15:34:31 +1000

    build a version that accept arguments

--- server.js
+++ server.js
@@ -3,24 +3,22 @@ var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
+// Int!'s '!' means can't be null
 var schema = buildSchema(`
   type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  quoteOfTheDay: () => {
-    return Math.random() < .5 ? "take it easy" : "salvation lies within";
-  },
-  random: () => {
-    return Math.random();
-  },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  rollDice: ({ numDice, numSides }) => {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+
+    return output;
   }
 };
 
